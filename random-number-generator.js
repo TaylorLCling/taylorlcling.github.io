0a209ec73c2f576f82237d8b778753b2b09dd1e3
TaylorLCling <TaylorClingenpeel@gmail.com>
2023-10-23 23:55:02 -0400

    Added DarkMode, Default Values, and an Animation to the Random Number generator

--- random-number-generator.js
+++ random-number-generator.js
@@ -1,11 +1,14 @@
+// First Auto Generate Button
 document.getElementById('btn1-3').addEventListener('click', function() {
     displayRandomNumber(1, 3);
 });
 
+// Second Auto Generate Button
 document.getElementById('btn1-4').addEventListener('click', function() {
     displayRandomNumber(1, 4);
 });
 
+// Random number generation classic
 document.getElementById('btnCustomRange').addEventListener('click', function() {
     const minValue = parseInt(document.getElementById('minValue').value);
     const maxValue = parseInt(document.getElementById('maxValue').value);
@@ -15,4 +18,37 @@ document.getElementById('btnCustomRange').addEventListener('click', function() {
 function displayRandomNumber(min, max) {
     const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
     document.getElementById('generatedNumber').innerText = randomNumber;
+
+    // Reset the animation
+    displayElement.style.animation = 'none';
+    // Force a reflow
+    void displayElement.offsetWidth;
+    // Reapply the animation
+    displayElement.style.animation = '';
+
+    displayElement.innerText = randomNumber;
+
+    // Vibrate for 200 milliseconds (only works on mobile devices that support the Vibration API)
+    if (navigator.vibrate) {
+        navigator.vibrate(200);
+    }
 }
+
+// Check for saved user preference and apply it
+if (localStorage.getItem('dark-mode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+}
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+    if (document.body.classList.contains('dark-mode')) {
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('dark-mode', 'disabled');
+    } else {
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('dark-mode', 'enabled');
+    }
+}
+
+// Attach the function to a button or other UI element
+document.getElementById('darkModeToggle').addEventListener('click', toggleDarkMode);
